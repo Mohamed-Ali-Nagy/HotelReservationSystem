2f88dff29a15c838e7cec80ef80419457ea139af
Mohamed Ahmed Salah Ibrahim <44170867+mohmedahmedsalah@users.noreply.github.com>
2024-08-16 20:29:21 +0300

    add repoistorty

--- HotelReservationSystem/Repositories/IRepository.cs
+++ HotelReservationSystem/Repositories/IRepository.cs
@@ -1,6 +1,18 @@
-﻿namespace HotelReservationSystem.Repositories
+﻿using System.Linq.Expressions;
+
+namespace HotelReservationSystem.Repositories
 {
     public interface IRepository<T>
     {
+        IQueryable<T> GetAll();
+        //IQueryable<T> Get(Expression<Func<T, bool>> predicate);
+        T GetByID(int id);
+        T Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        void Delete(int id);
+        //T First(Expression<Func<T, bool>> predicate);
+
+        void SaveChanges();
     }
 }
